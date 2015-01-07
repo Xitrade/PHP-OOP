9b488ebcace7e0024586d9d8e30bb302e53efb80
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-01-06 19:15:28 -0800

    added classes

--- lecture.php
+++ lecture.php
@@ -16,7 +16,28 @@
 	$door = new door();
 
 
+	class game{
+		public $name = "Kingdom Hearts";
+		public $genre = "JRPG, action, adventure";
+		public $publisher = "Square Enix"; 
+		public $platform = "Playstation 2";
+	}
+
+	class computer{
+		public $OS = "OSX Yosemite";
+		public $name = "Mac";
+		public $company = "Apple Inc.";
+	}
+
+	class anime{
+		public $name = "Attack On Titan";
+		public $genre = "terror, action, adveture";
+		public $year = "2013";
+	}
 	
+
 	
+
+
 
 ?>
