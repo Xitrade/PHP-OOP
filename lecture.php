ab691206fc7524e29a4a3ba0714175121338906c
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-01-06 19:02:38 -0800

    added code from example 1

--- lecture.php
+++ lecture.php
@@ -1,1 +1,22 @@
-testjkhkjkbhlkj
+<?php
+	class house{
+		//class body
+	}
+
+	class roof{
+		//class body
+	}
+
+	class door{
+		//class body
+	}
+
+	$house = new house();
+	$roof = new roof();
+	$door = new door();
+
+
+	
+	
+
+?>
