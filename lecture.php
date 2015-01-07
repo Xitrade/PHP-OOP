0091b5237125e2bec0ae51902359235fcda277c9
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-01-06 19:24:27 -0800

    attempted to do the methods

--- lecture.php
+++ lecture.php
@@ -31,12 +31,23 @@
 
 	class anime{
 		public $name = "Attack On Titan";
-		public $genre = "terror, action, adveture";
+		public $genre = "terror, action, adventure";
 		public $year = "2013";
 	}
 	
 
-	
+	class Cat {
+		public $name = "Max";
+		public $gender = "male";
+		public $price = 0;
+
+		function getName() {
+		return "{$this>name}";
+		}
+	}
+	$cat1 = new Cat();
+	$cat1->firstName = "Talon";
+	print "The cat’s name is {$cat1>getName()}.";
 
 
 
