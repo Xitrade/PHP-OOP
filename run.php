753fa5f03f2c166dbaf5af320e4d421079680379
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-01-08 12:27:27 -0800

    completed example 1

---
+++ run.php
@@ -0,0 +1,14 @@
+<?php
+	
+	//EXAMPLE 1
+
+	if (is_integer("69")) echo "This is numba"; 
+		else
+			echo "this no number";
+			var_dump(is_integer(7843));
+			var_dump(is_integer(789879));
+			var_dump(is_integer(96));
+			var_dump(is_integer(false));
+	
+
+?>
