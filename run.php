8021782f854b01509a0c48eda290f176ac9cd0d8
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-01-08 19:13:48 -0800

    added example 2

--- run.php
+++ run.php
@@ -1,6 +1,6 @@
 <?php
 	
-	//EXAMPLE 1
+//EXAMPLE 1
 
 	if (is_integer("69")) echo "This is numba"; 
 		else
@@ -10,5 +10,21 @@
 			var_dump(is_integer(96));
 			var_dump(is_integer(false));
 	
+//EXAMPLE 2
+
+	$trigger = true;
+	$marker = 0;
+
+	// Since $a is a boolean, it will return true
+	if (is_bool($trigger) === false) {
+	    echo "Yes, this is a boolean";
+	}
+
+	// Since $b is not a boolean, it will return false
+	if (is_bool($marker) === false) {
+	    echo "No, this is not a boolean";
+	}
+
+//EXAMPLE 3
 
 ?>
